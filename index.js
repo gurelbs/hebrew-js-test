a7a4dd470ee45dd90773a36d83de4bfe9ce7d57e
gurelbs <guri240@gmail.com>
2021-08-13 02:56:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const { Hebrew } = require('hebrew-js')
+const cors  = require('cors')
+const { json } = require('express')
+const app = express()
+
+app.use(cors())
+app.use(json())
+
+let answers = new Hebrew()
+
+app.post('/translate', async (req,res) => {
+  try {
+    let {word} = req.body
+    let answer = await answers.translate(word)
+    res.status(200).send({answer}).end()
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+app.listen(process.env.PORT || 5000,() => console.log(`server run at http://localhost:5000`))
+
+
+// direction('תל אביב','ירושלים')
+// news('קורונה')
+// translate('תרגם לאנגלית אפליקציה בעברית')
+// weather('פתח תקווה')
+// meaning('פילוסופיה')
