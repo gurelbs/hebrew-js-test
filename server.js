14c00c13ea1f2f7adee3403ad9d99b4bb424e769
gurelbs <guri240@gmail.com>
2021-08-13 04:57:18 +0300

    update

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
 const express = require('express')
 const { Hebrew } = require('hebrew-js')
-const cors  = require('cors')
-const app = express()
-const path = require("path");
+const cors = require('cors')
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '/client/build')));
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const answers = new Hebrew()
 
-let answers = new Hebrew()
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/client/build/index.html'));
-});
-
-app.post('/translate', async (req,res) => {
-  try {
-    let {word} = req.body
-    let answer = await answers.translate(word)
-    res.status(200).send({answer}).end()
-  } catch (error) {
-    console.log(error);
-  }
-})
-let PORT = process.env.PORT || 5000
-app.listen(PORT,() => console.log(`server run at http://localhost:${PORT}`))
-
-
-// direction('תל אביב','ירושלים')
-// news('קורונה')
-// translate('תרגם לאנגלית אפליקציה בעברית')
-// weather('פתח תקווה')
-// meaning('פילוסופיה')
+express()
+	.use(cors())
+	.use(express.json())
+	.use(express.static(path.join(__dirname, '/client/build')))
+	.get('/', (req, res) => res.sendFile(path.join(__dirname, '/client/build/index.html')))
+	.post('/translate', async (req, res) => {
+		try {
+			let { word } = req.body
+			let answer = await answers.translate(word)
+			res.status(200).send({ answer }).end()
+		} catch (error) {
+			console.log(error)
+		}
+	})
+	.listen(PORT, () => console.log(`server run at http://localhost:${PORT}`))
