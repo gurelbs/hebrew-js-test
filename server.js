b21cf586dbc6744d5283eaf9756fdf523b87f9b5
gurelbs <guri240@gmail.com>
2021-08-13 04:30:08 +0300

    update

--- server.js
+++ server.js
@@ -10,13 +10,10 @@ app.use(json())
 
 let answers = new Hebrew()
 
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
   app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname+'/client/build/index.html'));
   });
-}
-
 app.post('/translate', async (req,res) => {
   try {
     let {word} = req.body
